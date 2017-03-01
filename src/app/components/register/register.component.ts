b7e22fbb377f8471ac9b7aba0a6d77008a2f40b3
Danymill <justeam@gmail.com>
2017-03-01 10:42:17 +0300

    Feature: add registerForm layout to register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'hst-register',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = formBuilder.group({
+      userData: formBuilder.group({
+        username: ['', Validators.required],
+        login: ['', Validators.required],
+        email: ['',[
+          Validators.required,
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        ]],
+      }),
+      passwords: formBuilder.group({
+        password: ['', Validators.required],
+        rpassword: ['', Validators.required]
+      })
+    });
+  }
 
   ngOnInit() {
   }
