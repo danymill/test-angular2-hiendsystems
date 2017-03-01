32be3e23336147b60c222a50a5abc83f735d5faa
Danymill <justeam@gmail.com>
2017-03-01 10:14:38 +0300

    Feature: add dynamic page-title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRouteSnapshot, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'hst-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hst works!';
+export class AppComponent implements OnInit {
+  title: string;
+
+  constructor(private router: Router) {}
+
+  private getDeepestTitle(routeSnapshot: ActivatedRouteSnapshot) {
+    let title = routeSnapshot.data ? routeSnapshot.data['title'] : '';
+    if (routeSnapshot.firstChild) {
+      title = this.getDeepestTitle(routeSnapshot.firstChild) || title;
+    }
+    return title;
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe(
+      (event) => {
+        if (event instanceof NavigationEnd) {
+          this.title = this.getDeepestTitle(this.router.routerState.snapshot.root);
+        }
+      }
+    );
+  }
 }
