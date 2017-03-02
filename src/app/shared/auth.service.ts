659922133b650ce1498da05ae44a36047bf08449
Danymill <justeam@gmail.com>
2017-03-02 10:40:26 +0300

    Feature: add mockup logic to auth service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+
+import { User } from "./user.interface";
 
 @Injectable()
 export class AuthService {
+  public username: string;
 
   constructor() { }
 
-  register() {
-
+  register(registerData: User): Observable<any> {
+    let {username} = registerData;
+    return new Observable(
+      observer => {
+        setTimeout(() => {
+          this.username = username;
+          observer.next(username);
+        }, 3000);
+      }
+    );
   }
 }
